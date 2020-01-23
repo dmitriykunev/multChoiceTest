225ad2cdc2446c0c1c6b5063dd3da82787515a5f
Dmitriy <dmitriy.kunev@gmail.com>
2020-01-23 13:10:40 +0200

    new commit

--- Server/mult-test-server/src/auth/auth.service.ts
+++ Server/mult-test-server/src/auth/auth.service.ts
@@ -1,16 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService} from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly usersService: UsersService) {}
+    constructor(
+        private readonly usersService: UsersService,
+        private readonly jwtService: JwtService
+        ) {}
 
-    async validateUser(username: string, password: string): Promise<any> {
-        const user = await this.usersService.findOne(username);
+    async validateUser(userName: string, password: string): Promise<any> {
+        const user = await this.usersService.findOne(userName);
         if (user && user.password === password) {
             const { password, ...result } = user;
             return result;
         }
         return null;
+    }
+
+    async login(user: any) {
+        const payload = { userName: user.userName, sub:user.userId };
+        return {
+            access_token: this.jwtService.sign(payload),
+        };
     }
 }
